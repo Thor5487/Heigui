32a44ec6f91762b28b586ba7a7afe68811b7d4b6
Thor5487 <diego0218diego@gmail.com>
2026-09-13 01:49:09 +0800

    Deleted InstaMid because watchdog now ban InstaMidd

--- src/main/kotlin/com/iq200/heigui/features/ModuleManager.kt
+++ src/main/kotlin/com/iq200/heigui/features/ModuleManager.kt
@@ -21,7 +21,6 @@ import com.iq200.heigui.features.impl.dungeon.SkipSecrets
 import com.iq200.heigui.features.impl.dungeon.Triggerbot
 import com.iq200.heigui.features.impl.dungeon.ZPDB
 import com.iq200.heigui.features.impl.floor7.AutoCrit
-import com.iq200.heigui.features.impl.floor7.InstaMid
 import com.iq200.heigui.features.impl.floor7.LBHelper
 import com.iq200.heigui.features.impl.floor7.SimonSays
 import com.iq200.heigui.features.impl.floor7.WitherAimBot
@@ -75,7 +74,7 @@ object ModuleManager {
             SecretAura, AutoClose, ZPDB, Triggerbot, AutoClick, SecretDone, SkipSecrets, SATpFix, AutoCroesus,
 
             // floor 7
-            SimonSays, WitherAimBot, LBHelper, AutoCrit, InstaMid,
+            SimonSays, WitherAimBot, LBHelper, AutoCrit,
 
             // render
             ClickGUIModule,
